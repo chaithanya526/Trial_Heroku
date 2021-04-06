1e7879fed1743c1d3c8656f3ab4b0efce83aab50
Chaithanya Kumar G <chaithanyak526@gmail.com>
2021-04-06 17:21:50 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/EnteryClass.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EnteryClass {
+	
+	//@RequestMapping(value="/",method=RequestMethod.GET)
+	@GetMapping(value="/hello")
+	public String hello(){
+		return "Hello World wirtt get mapping";
+	}
+
+}
